8b3ab39bc0ec19f243664777b3e3524c271d8779
Carlos Nava <machinery.e@gmail.com>
2020-11-19 17:38:57 -0400

    prueba de nueva dependencia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataTablesNetComponent } from './tables/data-tables-net/data-tables-net.component';
+import { SmartTablesComponent } from './tables/smart-tables/smart-tables.component';
 
 const routes: Routes = [
   { path: 'data-tables-net', component: DataTablesNetComponent },
+  { path: 'smart-tables', component: SmartTablesComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
+import { Ng2CompleterModule } from '@akveo/ng2-completer';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 // Tables Packages
 import { DataTablesModule } from 'angular-datatables';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // Views
 import { HeaderComponent } from './layout/header/header.component';
 import { DataTablesNetComponent } from './tables/data-tables-net/data-tables-net.component';
+import { SmartTablesComponent } from './tables/smart-tables/smart-tables.component';
 
 
 
@@ -14,12 +17,15 @@ import { DataTablesNetComponent } from './tables/data-tables-net/data-tables-net
   declarations: [
     AppComponent,
     HeaderComponent,
-    DataTablesNetComponent
+    DataTablesNetComponent,
+    SmartTablesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DataTablesModule
+    DataTablesModule,
+    Ng2SmartTableModule,
+    Ng2CompleterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
